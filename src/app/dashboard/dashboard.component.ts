8cfd4a276388f3ee623bfbadf726fdf17b5837a9
Lorent02 <lorentjhuaalvaro327@gmail.com>
2022-10-21 15:10:13 +0700

    Mengirimkan_komponen

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private renderer : Renderer2) { }
+
+  ngAfterViewInit(): void
+  {
+    this.renderer.removeClass(document.body, "sidebar-open");
+    this.renderer.addClass(document.body, "sidebar-closed");
+  }
 
   ngOnInit(): void {
   }
